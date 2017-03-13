8e098fe89f087fa0c71633660ee013458230efa2
Ben Mabey <ben@benmabey.com>
2017-03-13 08:58:50 -0600

    make register_serializer part of the main public API

--- provenance/__init__.py
+++ provenance/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 from ._config import from_config, load_config, load_yaml_config
 from ._dependencies import dependencies
+from .serializers import register_serializer
 from .core import provenance, provenance_set, promote, archive_file
 from .repos import (capture_set, create_set, get_default_repo, set_run_info_fn,
                     get_set_by_id, get_set_by_name, lazy_dict, lazy_proxy_dict,
