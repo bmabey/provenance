e2ed4a4d68affd4f4d014ff1110175dfac841a4f
Ben Mabey <ben@benmabey.com>
2017-04-05 11:28:28 -0600

    make is_proxy part of the public API

--- provenance/__init__.py
+++ provenance/__init__.py
@@ -7,7 +7,7 @@ from .hashing import hash, value_repr
 from .repos import (capture_set, create_set, current_config, get_check_mutations,
                     get_default_repo, get_repo_by_name, get_set_by_id,
                     get_set_by_name, lazy_dict, lazy_proxy_dict, load_artifact,
-                    load_proxy, set_check_mutations, set_default_repo,
+                    load_proxy, is_proxy, set_check_mutations, set_default_repo,
                     set_run_info_fn, using_repo)
 from .serializers import register_serializer
 
